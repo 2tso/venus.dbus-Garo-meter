849ebe30b599b20baa67faebebcfaa05646d4e2f
2tso <134550599+2tso@users.noreply.github.com>
2024-08-13 21:48:55 +0200

    Update GARO-GNM3D.py

--- GARO-GNM3D.py
+++ GARO-GNM3D.py
@@ -14,28 +14,28 @@ class Reg_ver(Reg, int):
         v = values[0]
         return self.update((v >> 12, v >> 8 & 0xf, v & 0xff))
 
-nr_phases = [ 3, 3, 2, 1, 3 ]
+# nr_phases = [ 3, 3, 2, 1, 3 ]
 
-phase_configs = [
-    '3P.n',
-    '3P.1',
-    '2P',
-    '1P',
-    '3P',
-]
+#phase_configs = [
+#    '3P.n',
+#    '3P.1',
+#    '2P',
+#    '1P',
+#    '3P',
+#]
 
-switch_positions = [
-    'kVARh',
-    '2',
-    '1',
-    'Locked',
-]
+#switch_positions = [
+#    'kVARh',
+#    '2',
+#    '1',
+#    'Locked',
+#]
 
-class EM24_Meter(device.CustomName, device.EnergyMeter):
-    vendor_id = 'cg'
-    vendor_name = 'Carlo Gavazzi'
+class GNM3D_Meter(device.CustomName, device.EnergyMeter):
+    # vendor_id = 'cg'
+    # vendor_name = 'Carlo Gavazzi'
     productid = 0xb017
-    productname = 'Carlo Gavazzi EM24 Ethernet Energy Meter'
+    productname = 'Garo GNM3D Energy Meter'
     min_timeout = 0.5
 
     def phase_regs(self, n):
@@ -50,76 +50,43 @@ class EM24_Meter(device.CustomName, device.EnergyMeter):
     def device_init(self):
         self.info_regs = [
             Reg_ver( 0x0302, '/HardwareVersion'),
-            Reg_ver( 0x0304, '/FirmwareVersion'),
-            Reg_u16( 0x1002, '/PhaseConfig', text=phase_configs, write=(0, 4)),
+            Reg_ver( 0x0303, '/FirmwareVersion'),
+            # Reg_u16( 0x1002, '/PhaseConfig', text=phase_configs, write=(0, 4)),
             Reg_text(0x5000, 7, '/Serial'),
         ]
 
-        # make sure application is set to H
-        appreg = Reg_u16(0xa000)
-        if self.read_register(appreg) != 7:
-            self.write_register(appreg, 7)
-
-            # read back the value in case the setting is not accepted
-            # for some reason
-            if self.read_register(appreg) != 7:
-                self.log.error('%s: failed to set application to H', self)
-                return
-
         self.read_info()
 
-        phases = nr_phases[int(self.info['/PhaseConfig'])]
+        phases = 3 #nr_phases[int(self.info['/PhaseConfig'])]
 
         regs = [
             Reg_s32l(0x0028, '/Ac/Power',          10, '%.1f W'),
             Reg_u16( 0x0033, '/Ac/Frequency',      10, '%.1f Hz'),
             Reg_s32l(0x0034, '/Ac/Energy/Forward', 10, '%.1f kWh'),
-            Reg_s32l(0x004e, '/Ac/Energy/Reverse', 10, '%.1f kWh'),
-            Reg_u16( 0xa100, '/SwitchPos', text=switch_positions),
+            Reg_s32l(0x004e, '/Ac/Energy/Reverse', 10, '%.1f kWh'),            
         ]
-
-        if phases == 3:
-            regs += [
-                Reg_mapu16(0x0032, '/PhaseSequence', { 0: 0, 0xffff: 1 }),
-            ]
 
         for n in range(1, phases + 1):
             regs += self.phase_regs(n)
 
         self.data_regs = regs
-        self.nr_phases = phases
+        # self.nr_phases = phases
 
-    def dbus_write_register(self, reg, path, val):
-        super().dbus_write_register(reg, path, val)
-        self.sched_reinit()
+    #def dbus_write_register(self, reg, path, val):
+    #    super().dbus_write_register(reg, path, val)
+    #    self.sched_reinit()
 
 models = {
-    1648: {
-        'model':    'EM24DINAV23XE1X',
-        'handler':  EM24_Meter,
+    341: {
+        'model':    'GNM3D-RS485',
+        'handler':  GNM3D_Meter,
     },
-    1649: {
-        'model':    'EM24DINAV23XE1PFA',
-        'handler':  EM24_Meter,
-    },
-    1650: {
-        'model':    'EM24DINAV23XE1PFB',
-        'handler':  EM24_Meter,
-    },
-    1651: {
-        'model':    'EM24DINAV53XE1X',
-        'handler':  EM24_Meter,
-    },
-    1652: {
-        'model':    'EM24DINAV53XE1PFA',
-        'handler':  EM24_Meter,
-    },
-    1653: {
-        'model':    'EM24DINAV53XE1PFB',
-        'handler':  EM24_Meter,
-    },
+    342: {
+        'model':    'GNM3T-RS485',
+        'handler':  GNM3D_Meter,
+    },    
 }
 
 probe.add_handler(probe.ModelRegister(Reg_u16(0x000b), models,
-                                      methods=['tcp'],
+                                      methods=['tcp', 'rtu'],
                                       units=[1]))
