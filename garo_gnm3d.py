4dfa3946dc3b1c07df9af2148fdee4147981caa8
2tso <134550599+2tso@users.noreply.github.com>
2024-08-14 22:31:31 +0200

    Update garo_gnm3d.py

--- garo_gnm3d.py
+++ garo_gnm3d.py
@@ -1,28 +1,19 @@
+import logging
 import device
 import probe
 from register import *
 
-class Reg_ver(Reg, int):
-    def __init__(self, base, name):
-        super().__init__(base, 1, name, text='%d.%d.%d')
+log = logging.getLogger()
 
-    def __int__(self):
-        v = self.value
-        return v[0] << 16 | v[1] << 8 | v[2]
+nr_phases = [ 3, 3, 2, 1, 3 ]
 
-    def decode(self, values):
-        v = values[0]
-        return self.update((v >> 12, v >> 8 & 0xf, v & 0xff))
-
-# nr_phases = [ 3, 3, 2, 1, 3 ]
-
-#phase_configs = [
-#    '3P.n',
-#    '3P.1',
-#    '2P',
-#    '1P',
-#    '3P',
-#]
+phase_configs = [
+    '3P.n',
+    '3P.1',
+    '2P',
+    '1P',
+    '3P',
+]
 
 #switch_positions = [
 #    'kVARh',
@@ -34,10 +25,20 @@ class Reg_ver(Reg, int):
 class GNM3D_Meter(device.CustomName, device.EnergyMeter):
     # vendor_id = 'cg'
     # vendor_name = 'Carlo Gavazzi'
-    productid = 0xb017
+    productid = 0xFFFF
     productname = 'Garo GNM3D Energy Meter'
     min_timeout = 0.5
 
+     def __init__(self, *args):
+        super(GNM3D_Meter, self).__init__(*args)
+
+        self.info_regs = [
+            Reg_ver( 0x0302, '/HardwareVersion'),
+            Reg_ver( 0x0303, '/FirmwareVersion'),
+            # Reg_u16( 0x1002, '/PhaseConfig', text=phase_configs, write=(0, 4)),
+            Reg_text(0x5000, 7, '/Serial'),
+        ]
+    
     def phase_regs(self, n):
         s = 2 * (n - 1)
         return [
@@ -48,17 +49,10 @@ class GNM3D_Meter(device.CustomName, device.EnergyMeter):
         ]
 
     def device_init(self):
-        self.info_regs = [
-            Reg_ver( 0x0302, '/HardwareVersion'),
-            Reg_ver( 0x0303, '/FirmwareVersion'),
-            # Reg_u16( 0x1002, '/PhaseConfig', text=phase_configs, write=(0, 4)),
-            Reg_text(0x5000, 7, '/Serial'),
-        ]
-
+        log.info('Initializing Garo energy meter using connection "%s"', self.connection())
+       
         self.read_info()
-
         phases = 3 #nr_phases[int(self.info['/PhaseConfig'])]
-
         regs = [
             Reg_s32l(0x0028, '/Ac/Power',          10, '%.1f W'),
             Reg_u16( 0x0033, '/Ac/Frequency',      10, '%.1f Hz'),
