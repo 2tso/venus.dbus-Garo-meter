790c6f7ee900e482093ad12b04720577c95c6d08
2tso <134550599+2tso@users.noreply.github.com>
2024-08-16 22:21:57 +0200

    Update garo_gnm3d.py

--- garo_gnm3d.py
+++ garo_gnm3d.py
@@ -5,23 +5,23 @@ from register import *
 
 log = logging.getLogger()
 
-class Reg_text_serial(Reg, str):
-    def __init__(self, base, count, name=None, little=False, encoding=None, **kwargs):
-        super().__init__(base, count, name, **kwargs)
-        self.encoding = encoding or 'ascii'
-        self.pfmt = '%c%dH' % (['>', '<'][little], count)
+#class Reg_text_serial(Reg, str):
+#    def __init__(self, base, count, name=None, little=False, encoding=None, **kwargs):
+#        super().__init__(base, count, name, **kwargs)
+#        self.encoding = encoding or 'ascii'
+#        self.pfmt = '%c%dH' % (['>', '<'][little], count)
 
-    def decode(self, values):
-        log.info(values)
-        newval = struct.pack(self.pfmt, *values).rstrip(b'\0')
-        log.info(newval)
-        newval = str(newval.decode(encoding='ascii'))
-        log.info(newval)
-        return self.update(newval)
+ #   def decode(self, values):
+ ##       log.info(values)
+  #      newval = struct.pack(self.pfmt, *values).rstrip(b'\0')
+  #      log.info(newval)
+  #      newval = str(newval.decode(encoding='ascii'))
+  #      log.info(newval)
+  #      return self.update(newval)
 
-    def encode(self):
-        return struct.unpack(self.pfmt,
-            self.value.encode(self.encoding).ljust(2 * self.count, b'\0'))
+   # def encode(self):
+   #     return struct.unpack(self.pfmt,
+   #         self.value.encode(self.encoding).ljust(2 * self.count, b'\0'))
 
 
 
@@ -72,10 +72,11 @@ class GNM3D_Meter(device.EnergyMeter):
             Reg_u16( 0x0302, '/HardwareVersion'),
             Reg_u16( 0x0303, '/FirmwareVersion'),
             # Reg_u16( 0x1002, '/PhaseConfig', text=phase_configs, write=(0, 4)),
-            Reg_text_serial(0x5000, 7, '/Serial'),
+            Reg_text(0x5000, 7, '/Serial'),
         ]
         
-        #self.read_info()
+        self.read_info()
+        
         phases = 3 #nr_phases[int(self.info['/PhaseConfig'])]
         regs = [
             Reg_s32l(0x0028, '/Ac/Power',          10, '%.1f W'),
@@ -88,7 +89,7 @@ class GNM3D_Meter(device.EnergyMeter):
             regs += self.phase_regs(n)
 
         self.data_regs = regs
-        # self.nr_phases = phases
+        self.nr_phases = phases
     
     def get_ident(self):
         log.info('get_ident_returns cg_%s' % self.info['/Serial'])
